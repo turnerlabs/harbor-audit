b6e474651931cd5347658ff0f5fcb8b00499a01d
John Ritsema <john.ritsema@turner.com>
2016-08-14 00:17:11 -0400

    fixed bug in card expansion

--- src/ShipmentGroup.jsx
+++ src/ShipmentGroup.jsx
@@ -2,22 +2,55 @@ import React from 'react'
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card'
 import Shipment from './shipment.jsx'
 
-export default React.createClass({  
+export default React.createClass({
+
+  getInitialState() {
+    return {
+      expanded: false,
+      userSetExpanded: false,
+      newSearch: false,
+    }
+  },
+
+  componentWillReceiveProps(nextProps) {
+    //change the state if this is a new search
+    if (this.props.searchString !== nextProps.searchString)
+      this.setState({ 
+        newSearch: true,
+        userSetExpanded: false,
+      })
+  },
+
+  onExpandChange(newExpandedState) {
+    this.setState({ 
+      expanded: newExpandedState,
+      userSetExpanded: true,
+    })
+  },
+
   render() {    
     let { group, shipments, searchString } = this.props
+    let { expanded, userSetExpanded, newSearch } = this.state
 
     //filter by search string
     if (searchString && searchString.length > 1) {
       shipments = shipments.filter(s => s.name.indexOf(searchString) > -1)
     }
 
+    //don't render card if there are no search results
     if (shipments.length === 0)
       return <div />
 
+    //automatically expand card if there are new search results
+    //and user hasn't manually clicked to expand yet
+    if (searchString.length > 1 && newSearch && !userSetExpanded)
+      expanded = true
+
     return (
       <Card 
-        initiallyExpanded={false} 
-        expanded={(searchString.length > 1)}>
+        initiallyExpanded={false}
+        expanded={expanded} 
+        onExpandChange={this.onExpandChange}>        
         <CardHeader
           title={group}
           subtitle=""
