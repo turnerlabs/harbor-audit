85a7c1df378673c6f074b17aa814f0a63c56f790
John Ritsema <john.ritsema@turner.com>
2017-07-21 17:47:04 -0400

    start over

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-app.use(cors());
-
+app.use(cors())
 app.use(express.static(__dirname + '/public'))
 
 app.get('/health', (req, res) => {
